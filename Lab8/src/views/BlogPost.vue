dac242960b6c96fcec8601dc986dec1a23eebed7
YungP2008 <phamphu11102008@gmail.com>
2026-02-05 08:31:43 +0700

    nop lab 8

---
+++ Lab8/src/views/BlogPost.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.content }}</p>
+    <router-link to="/blog">Back to Blog List</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BlogPostView',
+  data() {
+    return {
+      posts: [
+        { id: 1, title: 'hello', content: 'toi la phu' },
+        { id: 2, title: 'hi', content: 'đep zai' },
+        { id: 3, title: 'xinchao', content: 'tam biet' }
+      ],
+      post: {}
+    }
+  },
+  created() {
+    const postId = parseInt(this.$route.params.id)
+    this.post = this.posts.find(p => p.id === postId) || {}
+  }
+}
+</script>
---
+++ Lab8/src/views/ProfileInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h3>Thông Tin</h3>
+    <p>Name: Phạm Châu Lương Phú</p>
+    <p>Email: phamphu11102008@gmail.com</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileInfoView'
+}
+</script>
